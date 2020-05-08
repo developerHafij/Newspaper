bcb2507f5142f94c7a605cb42bc3673705a8665c
Hafijur Rahman <developer.hafij@gmail.com>
2020-05-08 16:04:46 +0600

    Upload Newspaper website

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Newspaper</title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Include Header -->
+<?php include "header.php" ?>
+
+<!-- Include Menubar -->
+<?php include "menubar.php" ?>
+
+
+<section class="">
+	<div>
+		<p>This is demo Newspaper site</p>
+	</div>
+</section>
+
+
+<!-- Include Footer -->
+<?php include "footer.php" ?>
