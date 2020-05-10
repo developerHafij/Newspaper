ef96690ae0f3576ed902c3b374c084b4e87c7141
Hafijur Rahman <developer.hafij@gmail.com>
2020-05-10 14:48:35 +0600

    Update Article Section

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Volkhov:wght@400;700&display=swap" rel="stylesheet">
     <!-- Font Awesome CDN -->
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
--- index.php
+++ index.php
@@ -2,14 +2,275 @@
 <?php include "header.php" ?>
 
 <!-- Include Menubar -->
-<?php include "menubar.php" ?>
+<?php include "navbar.php" ?>
 
 
-<section class="">
-	<div>
-		<p>This is demo Newspaper site</p>
+<!-- Start Economical Impact Section -->
+<section class="eco-section" style="padding-top: 65px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 emp-left-items">
+				<!-- Left Items Start -->
+				<div class="emp-title">
+					<div class="emp-title-head">
+						<h2>ECONOMICAL IMPACT</h2>
+					</div>
+					<div class="emp-border"></div>
+					<p class="emp-filter">Others <i class="fa fa-angle-down"></i></p>
+				</div>
+				<div class="row emp-descrip">
+					<div class="col-md-6 left-des">
+						<a href="#">
+							<div class="emp-item1 cmn-emp-items">
+								<img src="images/36-696x464.jpg">
+								<div class="emp-info">
+									<span>HEATHER GRAY -MARCH 19, 2020</span>
+									<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6 right-des">
+						<a href="">
+							<div class="emp-item2 cmn-emp-items">
+								<img src="images/35-696x464.jpg">
+								<div class="emp-info">
+									<span>HEATHER GRAY -MARCH 20, 2020</span>
+									<h5>Witnessing the Birth of the New Coronavirus Economy</h5>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6 left-des">
+						<a href="">
+							<div class="emp-item3 cmn-emp-items">
+								<img src="images/34-696x464.jpg">
+								<div class="emp-info">
+									<span>HEATHER GRAY -MARCH 19, 2020</span>
+									<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6 right-des">
+						<a href="">
+							<div class="emp-item4 cmn-emp-items">
+								<img src="images/33-696x464.jpg">
+								<div class="emp-info">
+									<span>HEATHER GRAY -MARCH 19, 2020</span>
+									<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6 left-des">
+						<a href="">
+							<div class="emp-item4 cmn-emp-items">
+								<img src="images/32-696x464.jpg">
+								<div class="emp-info">
+									<span>HEATHER GRAY -MARCH 19, 2020</span>
+									<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6 right-des">
+						<a href="">
+							<div class="emp-item4 cmn-emp-items">
+								<img src="images/31-696x464.jpg">
+								<div class="emp-info">
+									<span>HEATHER GRAY -MARCH 19, 2020</span>
+									<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-12 load-more">
+						<h6>Load More <i class="fa fa-angle-down"></i></h6>
+					</div>
+				</div>
+				<!-- Left Items End -->
+			</div>
+
+			<!-- Right Items Start -->
+			<div class="col-md-4">
+				<div class="emp-right-items">
+					<div class="emp-social">
+						<div class="emp-fb emp-soci">
+							<div class="fa fa-facebook"></div>
+							<p class="emp-social-count cnt">2,5623</p>
+							<p class="emp-social-name nm">Fan</p>
+						</div>
+						<div class="emp-tw emp-soci">
+							<div class="fa fa-twitter"></div>
+							<p class="emp-social-count cnt">2,5623</p>
+							<p class="emp-social-name nm">Followers</p>
+						</div>
+						<div class="emp-yt emp-soci">
+							<div class="fa fa-youtube"></div>
+							<p class="emp-social-count cnt">1,574</p>
+							<p class="emp-social-name nm">Subscribers</p>
+						</div>
+					</div>
+
+					<div class="facts">
+						<div class="facts-title">
+							<span>Facts</span>
+							<div class="facts-arrow"></div>
+						</div>
+					</div>
+					<div class="facts-details">
+						<div class="facts-de-item1 facts-de">
+							<div class="facts-de-img">
+								<a href=""><img src="images/6-696x464.jpg" width="95px;" title="The Coronavirus Epidemic Might Be Much Worse Than We Thought"></a>
+							</div>
+							<div class="facts-right">
+								<h6>The Coronavirus Epidemic Might Be Much Worse Than We Thought</h6>
+								<span>MARCH 19, 2020</span>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+			</div>
+			<!-- Right Items End -->
+
+		</div>
 	</div>
 </section>
+<!-- End Economical Impact Section -->
+
+
+<!-- Start Latest Article Section -->
+<section class="article-section" style="padding-top: 45px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 a-left-items">
+				<!-- Left Items Start -->
+				<div class="row">
+					<div class="col-md-12 a-title">
+						<div class="a-title-head">
+							<h2>LATEST ARTICLES</h2>
+						</div>
+						<div class="a-border"></div>
+					</div>
+					<div class="col-md-12 a-items">
+						<a href="" title="Clinical Research Fails to Provide a Complete COVID19 Pattern">
+							<div class="col-md-6 a-left-img">
+								<img src="images/60-696x464.jpg">
+							</div>
+							<div class="col-md-6 a-left-details">
+								<h6>RESEARCH</h6>
+								<h5>Survey Reveals Significant Need for Increased Consumer Education</h5>
+								<span>HEATHER GRAY - MARCH 19, 2020</span>
+								<p>Sugar seems to have developed a reputation as the big bad wolf in relation to health. We have reported on numerous studies associating sugar.</p>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-12 a-items">
+						<a href="" title="Clinical Research Fails to Provide a Complete COVID19 Pattern">
+							<div class="col-md-6 a-left-img">
+								<img src="images/58-696x464.jpg">
+							</div>
+							<div class="col-md-6 a-left-details">
+								<h6>RESEARCH</h6>
+								<h5>Anxiety Research: Your Nose Itches: Allergies, Flu or COVID19</h5>
+								<span>HEATHER GRAY - MARCH 19, 2020</span>
+								<p>Sugar seems to have developed a reputation as the big bad wolf in relation to health. We have reported on numerous studies associating sugar.</p>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-12 a-items">
+						<a href="" title="Clinical Research Fails to Provide a Complete COVID19 Pattern">
+							<div class="col-md-6 a-left-img">
+								<img src="images/1-696x464.jpg">
+							</div>
+							<div class="col-md-6 a-left-details">
+								<h6>RESEARCH</h6>
+								<h5>Clinical Research Fails to Provide a Complete COVID19 Pattern</h5>
+								<span>HEATHER GRAY - MARCH 19, 2020</span>
+								<p>Sugar seems to have developed a reputation as the big bad wolf in relation to health. We have reported on numerous studies associating sugar.</p>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-12 a-items">
+						<a href="" title="Clinical Research Fails to Provide a Complete COVID19 Pattern">
+							<div class="col-md-6 a-left-img">
+								<img src="images/44-696x464.jpg">
+							</div>
+							<div class="col-md-6 a-left-details">
+								<h6>RESEARCH</h6>
+								<h5>Clinical Research Fails to Provide a Complete COVID19 Pattern</h5>
+								<span>HEATHER GRAY - MARCH 19, 2020</span>
+								<p>Sugar seems to have developed a reputation as the big bad wolf in relation to health. We have reported on numerous studies associating sugar.</p>
+							</div>
+						</a>
+					</div>
+
+				</div>
+			</div>
+			<!-- Left Items End -->
+
+			<!-- Right Items Start -->
+			<div class="col-md-4 a-right-items">
+				<div class="row">
+					<div class="col-md-12 topics">
+						<div class="topics-title">
+							<span>Hot Topics</span>
+							<div class="topics-arrow"></div>
+						</div>
+					</div>
+					<div class="topics-descrip">
+						<div class="col-md-12 des-padding">
+							<a href="#" title="Companies Are Putting Profits Ahead of Public Health">
+								<div class="cmn-topics-items">
+									<img src="images/54-696x464.jpg">
+									<div class="topics-info">
+										<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 des-padding">
+							<a href="#">
+								<div class="cmn-topics-items">
+									<img src="images/53-696x464.jpg">
+									<div class="topics-info">
+										<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 des-padding">
+							<a href="#">
+								<div class="cmn-topics-items">
+									<img src="images/52-696x464.jpg">
+									<div class="topics-info">
+										<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-md-12 des-padding">
+							<a href="#">
+								<div class="cmn-topics-items">
+									<img src="images/36-696x464.jpg">
+									<div class="topics-info">
+										<h5>Companies Are Putting Profits Ahead of Public Health</h5>
+									</div>
+								</div>
+							</a>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<!-- Right Items End -->
+		</div>
+	</div>
+</section>
+<!-- End Latest Article Section -->
 
 
 <!-- Include Footer -->
